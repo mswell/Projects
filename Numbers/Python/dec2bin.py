8bcf9927adb7d35d701de20c1f37b86e8af6622f
alexandre <aesouza.jvm@gmail.com>
2014-07-12 20:57:43 -0300

    update

--- Numbers/Python/dec2bin.py
+++ Numbers/Python/dec2bin.py
@@ -4,6 +4,6 @@ def dec2bin(num):
     Exemplo de um simples conversor de decimal para binário.
     '''
     (num/2 > 0 and (dec2bin(int(num/2)), conv.append(num%2)))
-    print(''.join(map(str, conv)))
 
 dec2bin(int(input('Decimal: ')))
+print(''.join(map(str, conv)))
