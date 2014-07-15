73803f74564bdd54ebebc876de66554b1783969f
alexandre <aesouza.jvm@gmail.com>
2014-07-14 23:52:00 -0300

    python solutions

---
+++ Text/Python/reverse_str.py
@@ -0,0 +1,9 @@
+def reverte_str(string):
+    '''
+    Enter a string and the program will reverse it and print it out.
+    >>> reverte_str('oi')
+    'io'
+    >>> reverte_str('abaab')
+    'baaba'
+    '''
+    return string[::-1]
